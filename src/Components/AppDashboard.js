749716660f10f95c139b3d1adffab4230a6ffe41
Scott Reyell <smreyell@gmail.com>
2020-05-18 11:01:09 -0700

    adding Marios changes to be tracked by a remote branch

--- src/Components/AppDashboard.js
+++ src/Components/AppDashboard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import AppLinks from './AppLinks';
+// import AppLinks from './AppLinks';
+import AppLinks2 from './AppLinks2';
 import AppDescription from './AppDescription';
 
 export default function AppDashboard({ logo, description, pm, email, phone }) {
 	return (
 		<Grid container style={{ backgroundColor: '#81d4fa' }}>
 			<AppDescription logo={logo} description={description} pm={pm} email={email} phone={phone} />
-			<AppLinks />
+			<AppLinks2 />
 		</Grid>
 	);
 }
